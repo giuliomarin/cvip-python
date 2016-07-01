96d5afd75c9ee00029928dcd391a4a62a94f052a
Giulio Marin <giulio.marin@me.com>
2016-07-01 11:03:33 -0700

    video list for Aquifi videos

--- test/creategif.py
+++ test/creategif.py
@@ -1,11 +1,29 @@
 from moviepy.editor import *
+import os
 
-inPath = '../../samples/images/%s'
-imglist = ['left.png', 'right.png']
+# Create gif for Aquifi videos
+if False:
+    mainfolder = '/Users/giulio/Dropbox (Aquifi)/Data/ScanGtRegTestData/%s'
+    for folder in os.listdir(mainfolder % ''):
+        if os.path.isfile(mainfolder % folder):
+            continue
 
-images = []
-for i in imglist:
-    images.append(inPath % i)
-    
-clip = ImageSequenceClip(images, fps=2).crop(x1=400, x2=1100, y1=500, y2 = 1000).resize(0.5)
-clip.write_gif(inPath % 'sample.gif')
+        inPath = mainfolder % folder + '/Frames/%s'
+        if not os.path.exists(inPath % ''):
+            inPath = mainfolder % folder + '/sir/Frames/%s'
+        imgpath = inPath % 'color0_%d.png'
+
+        if os.path.exists(inPath % '../gif.gif'):
+            continue
+
+        nframes = len([name for name in os.listdir(inPath % '') if os.path.isfile(inPath % name) and name.startswith('color0')])
+        imglist = range(1, nframes, int(nframes / 50.0))
+
+        images = []
+        for i in imglist:
+            images.append(imgpath % i)
+
+        clip = ImageSequenceClip(images, fps=15)
+        # clip.crop(x1=400, x2=1100, y1=500, y2 = 1000)
+        clip = clip.resize(1.0 / 6.0)
+        clip.write_gif(inPath % '../gif.gif')
