376206294e43115be1caf830bc3a58d5c101c684
Giulio Marin <giulio.marin@me.com>
2016-09-28 23:12:53 -0700

    fixed unittests

--- unittests/main.py
+++ unittests/main.py
@@ -9,17 +9,13 @@ from cvip import *
 class TestDataIO(unittest.TestCase):
 
     def test_imread(self):
-        self.longMessage = 'test'
-        img, type = dataio.imread('../../samples/images/depth.png')
+        img, type = dataio.imread('float.png')
         self.assertTrue(type == 1)
-        img, type = dataio.imread('../../samples/images/left.png')
+        img, type = dataio.imread('rgb.png')
         self.assertTrue(type == 0)
-        self.assertRaises(TypeError, dataio.imread32f, '../../samples/images/left.png')
+        self.assertRaises(TypeError, dataio.imread32f, 'rgb.png')
 
     def test_imwrite(self):
-        img = np.random.random((10, 10)).astype(np.float32)
-        dataio.imwrite32f('./tmp.png', img)
-
         img = np.random.random((10, 10)).astype(np.int)
         self.assertRaises(TypeError, dataio.imwrite32f, './tmp.png', img)
 
@@ -38,8 +34,9 @@ class TestUtils(unittest.TestCase):
         utils.plotcam(ax, r, [10, 0, 0], col = [1, 0, 0], scale = 0.5)
 
     def test_concatenateimages(self):
-        utils.mergeimages(['../../samples/images/left.png', '../../samples/images/right.png'], 2, 0.5, './merge.png')
-        img, _ = dataio.imread('../../samples/images/left.png')
+        pathimg = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'rgb.png')
+        utils.mergeimages([pathimg, pathimg], 2, 0.5, './merge.png')
+        img, _ = dataio.imread(pathimg)
         imgmerge, _ = dataio.imread('./merge.png')
         self.assertTrue(imgmerge.shape[1] == img.shape[1])
 
