6b1cea9031eb0f1842cf73b13188376164090087
Giulio Marin <giulio.marin@me.com>
2016-11-02 15:40:59 +0100

    crop and align image

--- cvip/utils.py
+++ cvip/utils.py
@@ -122,6 +122,64 @@ def mergeimages(imagesList, numCols, resize = 1.0, nameOut = None):
     return imgMerge
 
 
+def cropimage(img, tlbr):
+
+    # TODO: handle different roi input and grayscale images
+    tl = tlbr[0]
+    br = tlbr[1]
+    img_crop = img[tl[0]:br[0], tl[1]:br[1], :]
+    return img_crop
+
+
+def alignimage(ref, img):
+    # Convert images to grayscale
+    if (len(ref.shape) > 2) & (ref.shape[2] == 3):
+        im1_gray = cv2.cvtColor(ref, cv2.COLOR_BGR2GRAY)
+    else:
+        im1_gray = ref
+    if (len(img.shape) > 2) & (img.shape[2] == 3):
+        im2_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    else:
+        im2_gray = img
+
+    # Find size of image1
+    sz = ref.shape
+
+    # Define the motion model
+    warp_mode = cv2.MOTION_HOMOGRAPHY
+
+    # Define 2x3 or 3x3 matrices and initialize the matrix to identity
+    if warp_mode == cv2.MOTION_HOMOGRAPHY:
+        warp_matrix = np.eye(3, 3, dtype = np.float32)
+    else:
+        warp_matrix = np.eye(2, 3, dtype = np.float32)
+
+    # Specify the number of iterations.
+    number_of_iterations = 1000
+
+    # Specify the threshold of the increment
+    # in the correlation coefficient between two iterations
+    termination_eps = 1e-6
+
+    # Define termination criteria
+    criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, number_of_iterations, termination_eps)
+
+    # Run the ECC algorithm. The results are stored in warp_matrix.
+    (cc, warp_matrix) = cv2.findTransformECC(im1_gray, im2_gray, warp_matrix, warp_mode, criteria)
+
+    if warp_mode == cv2.MOTION_HOMOGRAPHY:
+        # Use warpPerspective for Homography
+        im2_aligned = cv2.warpPerspective(img, warp_matrix, (sz[1], sz[0]),
+                                          flags = cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP)
+    else:
+        # Use warpAffine for Translation, Euclidean and Affine
+        im2_aligned = cv2.warpAffine(img, warp_matrix, (sz[1], sz[0]), flags = cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP)
+
+    # TODO: handle exception if function does not converge
+
+    return im2_aligned
+
+
 if __name__ == '__main__':
 
     # Test merge images
